1c744400e7b54695bf58ba41d7980daaccf90a53
Himmat Magar <himmatmagar007@gmail.com>
2026-05-22 15:58:23 +0545

    fastapi endpoint added

--- api/app.py
+++ api/app.py
@@ -4,23 +4,40 @@ from fastapi import FastAPI
 from typing import Annotated, Literal
 from pydantic import BaseModel, Field
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-with open('model.pkl', 'rb') as f:
+with open('artifact/model_building/model.pkl', 'rb') as f:
     model = jb.load(f)
+    
+with open("artifact/data_transformation/preprocessor.pkl", 'rb') as f:
+    processor = jb.load(f)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get('/')
-def home():
+def root():
     return {'message': 'Hello'}
+
+@app.get("/health")
+def health():
+    return {"status": "OK"}
 
 
 class UserInput(BaseModel):
     time_spent_alone: Annotated[float, Field(..., gt=0, description='time spent alone')]
-    stage_fear: Annotated[Literal['Yes', 'No'], Field(..., description='You have stage fear or not')]
+    stage_fear: Annotated[Literal['yes', 'no'], Field(..., description='You have stage fear or not')]
     social_event_attendance: Annotated[float, Field(..., gt=0, description='Social event attendance')]
     going_outside: Annotated[float, Field(..., gt=0, description='Going outside')]
-    drained_after_socializing: Annotated[Literal['Yes', 'No'], Field(..., description='Have you ever drained after socializing')]
+    drained_after_socializing: Annotated[Literal['yes', 'no'], Field(..., description='Have you ever drained after socializing')]
     friends_circle_size: Annotated[float, Field(..., gt=0, description='Friend circle size')]
     post_frequency: Annotated[float, Field(..., gt=0, description='Post Frequency')]
 
@@ -38,5 +55,6 @@ def prediction(user_data: UserInput):
         'post_frequency': user_data.post_frequency
     }])
 
-    prediction = model.predict(input)
-    raise JSONResponse(200, content= {'prediction': prediction})
+    input_transforms = processor.transform(input)
+    prediction = model.predict(input_transforms)
+    return JSONResponse(content={'prediction': prediction.tolist()}, status_code=200)
