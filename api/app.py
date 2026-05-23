1e2125d7efc7bd05a4d9e561fd571f2251f9b81a
Himmat Magar <himmatmagar007@gmail.com>
2026-05-23 16:57:28 +0545

    fixing api bugs

--- api/app.py
+++ api/app.py
@@ -34,10 +34,10 @@ def health():
 
 class UserInput(BaseModel):
     time_spent_alone: Annotated[float, Field(..., gt=0, description='time spent alone')]
-    stage_fear: Annotated[Literal['yes', 'no'], Field(..., description='You have stage fear or not')]
+    stage_fear: Annotated[Literal['Yes', 'No'], Field(..., description='You have stage fear or not')]
     social_event_attendance: Annotated[float, Field(..., gt=0, description='Social event attendance')]
     going_outside: Annotated[float, Field(..., gt=0, description='Going outside')]
-    drained_after_socializing: Annotated[Literal['yes', 'no'], Field(..., description='Have you ever drained after socializing')]
+    drained_after_socializing: Annotated[Literal['Yes', 'No'], Field(..., description='Have you ever drained after socializing')]
     friends_circle_size: Annotated[float, Field(..., gt=0, description='Friend circle size')]
     post_frequency: Annotated[float, Field(..., gt=0, description='Post Frequency')]
 
